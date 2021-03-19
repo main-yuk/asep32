a412dccd58c26bb8f748ab73770db7fd789e0ec3
asep <githubasep@gmail.com>
2021-03-19 09:36:03 +0700

    tugas_1

--- index.js
+++ index.js
@@ -2,56 +2,35 @@ const express = require('express');
 
 const app = express();
 
-app.get('/home', (req, res) => {
 
-	res.send('homepage menu');
-
-});
-
-app.get('/hehe', (req, res) => {
-	res.send("ini ketawa");
-});
-
-app.get('/api/jasa', (req, res) => {
-	res.json({
-		message: "mie sukses isi 2",
-		name: "sule",
-		img: "hahaha.jpg",
-		price: "90000",
-		tag: null
-	});
-});
-
-app.get('/api/cok', (req, res) => {
+app.get('/vt/reine', (req, res) => {
 	res.json({
 		message: "kalkun",
-		name: "sule",
-		img: "hahaha.jpg",
-		price: "90000",
+		name: "vtuber",
+		img: "wkwkwk.jpg",
 		tag: null
 	});
 });
 
-app.get('/api/kazuma', (req, res) => {
+app.get('/vt/bulan', (req, res) => {
 	res.json({
-		message: "lol",
-		name: "sule",
-		img: "hahaha.jpg",
-		price: "90000",
+		message: "moona",
+		name: "vtuber",
+		img: "hehe.jpg",
 		tag: null
 	});
 });
 
-app.get('/api/deku', (req, res) => {
-	res.json({
-		message: "boku no hero",
-		name: "sule",
-		img: "hahaha.jpg",
-		price: "90000",
-		tag: null
-	});
+app.get('/login', (req, res) => {
+
+	res.send('successfully');
+
 });
 
-app.listen(9000, () => {
+app.get('/logout', (req, res) => {
+	res.send("successfully");
+});
+
+app.listen(2000, () => {
 	console.log('server running on port 9000');
 });
